6c84a9ebd5a441e782026d5e8b6fb4c93246a20a
Levelasquez15 <lewis.velasquezwatts@unicolombo.edu.co>
2025-11-26 00:18:38 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'AR-aplication-taller',
+  webDir: 'www'
+};
+
+export default config;
